7814b1979416160000399aba6a0cad909ccefaf9
7238 唐翊傑 ejtang <11305001@pershing.com.tw>
2024-10-15 10:58:21 +0800

    feat: open ai

--- src/js/common/views/ChatInput.jsx
+++ src/js/common/views/ChatInput.jsx
@@ -1,21 +1,39 @@
-import React from 'react';
+import React, { useCallback, useState } from 'react';
+import PropTypes from 'prop-types';
 
-function ChatInput() {
+function ChatInput({setMsgList}) {
+
+  const [message, setMessage] = useState('');
+
+  const handleSend = useCallback((e) => {
+    e.preventDefault();
+    if (message) {
+      setMsgList((prev) => [...prev, {text: message, type: 'user'}]);
+      setMessage('');
+    }
+  },[message, setMsgList]);
+
   return (
     <div className="position-fixed bottom-0 w-100 p-3 bg-light">
-      <div className="input-group">
+      <form className="input-group" onSubmit={handleSend}>
         <input
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
           type="text"
           className="form-control"
           placeholder="Type a message"
           aria-label="Message input"
         />
-        <button className="btn btn-primary" type="button">
+        <button className="btn btn-primary" type="button" onClick={handleSend}>
           Send
         </button>
-      </div>
+      </form>
     </div>
   );
 }
+
+ChatInput.propTypes = {
+  setMsgList: PropTypes.func,
+};
 
 export default ChatInput;
